ee28046c0c3bcf57e618cd5f019fd9384a8a3275
yuexianguo <gyx685255>
2017-03-29 21:35:31 +0800

    9.专题页面加载数据，创建subjectAdapter加载其相应的item布局；

--- app/src/main/java/com/example/administrator/googleplayer/ui/fragment/SubjectFragment.java
+++ app/src/main/java/com/example/administrator/googleplayer/ui/fragment/SubjectFragment.java
@@ -1,20 +1,59 @@
 package com.example.administrator.googleplayer.ui.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.administrator.googleplayer.adapter.SubjectAdapter;
+import com.example.administrator.googleplayer.bean.SubjectItemBean;
+import com.example.administrator.googleplayer.network.HeiMaRetrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Administrator on 2017/3/27.
  */
 
-public class SubjectFragment extends Fragment {
-    @Nullable
+public class SubjectFragment extends BaseLoadmoreListFragment {
+
+    private static final String TAG = "SubjectFragment";
+
+    private List<SubjectItemBean> mDataList;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return null;
+    public BaseAdapter onCreatAdapter() {
+        return new SubjectAdapter(getContext(),mDataList);
+    }
+
+
+    @Override
+    protected void startLoadData() {
+
+        Call<List<SubjectItemBean>> listCall =  HeiMaRetrofit.getInstrance().getApi().listSubject(0);
+        listCall.enqueue(new Callback<List<SubjectItemBean>>() {
+            @Override
+            public void onResponse(Call<List<SubjectItemBean>> call, Response<List<SubjectItemBean>> response) {
+                //Log.d(TAG, "onResponse: "+response.body());
+                //Toast.makeText(getContext(),"请求结果"+response.body(),Toast.LENGTH_SHORT).show();
+                mDataList=response.body();
+                onDataLoadSuccess();
+
+            }
+
+            @Override
+            public void onFailure(Call<List<SubjectItemBean>> call, Throwable t) {
+                onDataLoadFailed();
+            }
+        });
+
+    }
+
+    @Override
+    protected void startLoadMoreData() {
+
+
     }
 }
